1f6f377be94a386532400dbb6d328290a82215ca
Vishal Patel <vishalpatel@Vishals-MacBook-Air.local>
2024-03-18 00:40:12 +0530

    Setup AWS cognito Identity provider

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 const app = express();
-import AWS from 'aws-sdk';
 import route from './routes/routes.js';
-import config from './config/config.js';
 import bodyParser from 'body-parser';
-import path from 'path';
-
-AWS.config.credentials = new AWS.Credentials(
-  config.AWS.accessKeyId, // Your access key ID
-  config.AWS.secretAccessKey, // Your secret access key
-);
-AWS.config.region = config.AWS.region;
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -24,16 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use("/", route);
-
-const { MongoClient } = require('mongodb')
-
-// Create Instance of MongoClient for mongodb
-const client = new MongoClient('mongodb://localhost:27017')
-
-// Connect to database
-client.connect()
-.then(() => console.log('Connected Successfully'))
-.catch(error => console.log('Failed to connect', error))
 
 app.listen(3000, async function () {
   console.log('Example app listening on port 3000!');
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
  
 import { Router } from 'express'; 
-const app = Router(); 
+const app = Router();
+import config from '../config/config.js';
+import { CognitoIdentityProviderClient, ListUsersCommand } from "@aws-sdk/client-cognito-identity-provider"; 
+const client = new CognitoIdentityProviderClient({ 
+    "accessKeyId": config.AWS.accessKeyId, // Your access key ID
+    "secretKeyId": config.AWS.secretAccessKey, // Your secret access key
+    "region": "ap-south-1" });
+
 
 app.get('/signup', (req, res) => { 
     res.render('signup'); 
@@ -10,8 +17,19 @@ app.get('/', (req, res) => {
     res.render('login'); 
 });
 
-app.post('/', (req, res) => { 
-    console.log('req received', req.body);
+app.post('/signup', async (req, res) => { 
+    try {
+        // a client can be shared by different commands
+        const input = { // ListUsersRequest
+            UserPoolId: "ap-south-1_xcxMV5mzj", // required
+        };
+        const command = new ListUsersCommand(input);
+        const response = await client.send(command);
+        console.log('users', JSON.stringify(response, null, 4))
+    }
+    catch(error){
+        console.error('Error from signup route ', error);
+    }
 });
   
 export default app;
